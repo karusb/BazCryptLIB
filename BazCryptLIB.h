19b57f466cc2a7c1093db04125ad8e3030adec23
karusb <sokucam@hotmail.com>
2017-11-07 19:19:17 +0300

    Initial Cleanup

--- BazCryptLIB.h
+++
@@ -1,6 +0,0 @@
-
-void evolve39318(bitset<20000> &s, int nbytes);
-void evolve57630z(bitset<20000> &s, int nbytes);
-void evolve57630b(bitset<20000> &s, int nbytes);
-int* pokertest(bitset<8> &s);
-string BazCrypt(string MESSAGE, string password, int generations, int algorithm = 0, bool verbose = false);
